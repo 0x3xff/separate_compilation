c477bb45b8400e0a801eb5a3a7f4c1f2bed951a2
0x00 <98767059+0x3xff@users.noreply.github.com>
2025-11-17 11:47:28 +0200

    commit

---
+++ file1.cpp
@@ -0,0 +1,7 @@
+#include "file1.h"
+#include <iostream>
+
+int func1(){
+    std::cout << "func1 called" << std::endl;
+    return 0;
+}
---
+++ file2.cpp
@@ -0,0 +1,5 @@
+#include "file1.h"
+
+int main(){
+    func1();
+}
